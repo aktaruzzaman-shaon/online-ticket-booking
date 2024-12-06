906661c58d9cd2d75a5fd3da8a23f21bc01d9196
aktaruzzaman-shaon <aktaruzzamanshaon69@gmail.com>
2024-12-06 21:52:40 +0600

    added component file  and folder

---
+++ src/Components/shared/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Login from '../Login/Login'
+import Information from '../Information/Information'
+import Contact from '../Contact/Contact'
+import Home from '../Home'
+
+const Navbar = () => {
+  return (
+    <>
+      <div>Navbar</div>
+    </>
+  )
+}
+
+export default Navbar
