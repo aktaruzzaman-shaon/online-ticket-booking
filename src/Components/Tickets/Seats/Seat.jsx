d5248dee023ffb89398f8c8651e397a03cd59c61
aktaruzzaman-shaon <aktaruzzamanshaon69@gmail.com>
2024-12-10 19:08:13 +0600

    seat selection

--- src/Components/Tickets/Seats/Seat.jsx
+++ src/Components/Tickets/Seats/Seat.jsx
@@ -1,9 +1,9 @@
 import React, { useRef, useState } from 'react'
 
 const Seat = (props) => {
+
     const { seatQuantity } = props;
-    // const [selectColor, setSelectColor] = useState(null)
-    const seatRef = useRef(null)
+    const [selectedSeat, setSelectedSeat] = useState()
 
     const seatsArray = [];
 
@@ -11,20 +11,21 @@ const Seat = (props) => {
         seatsArray.push(i)
     }
 
-    const seatBookingHandler = (e) => {
-        e.preventDefault()
-        const selectedSeat = seatRef.current;
-        selectedSeat.backgroundColor = "red"
-        console.log(selectedSeat)
+    const seatBookingHandler = (index) => {
+        setSelectedSeat(index)
+        setTimeout(() => {
+            setSelectedSeat(null)
+        }, 9000);
     }
 
     return (
         <>
             <div className='flex flex-row mx-auto justify-center space-x-1 mt-5'>
                 {
-                    seatsArray.map((seat) => <div ref={seatRef} className={`p-2  w-20 rounded-md `} onClick={(e) => seatBookingHandler(e)} key={seat}>{seat}</div>)
+                    seatsArray.map((seat, index) => <div className={`p-2  w-20 rounded-md ${selectedSeat === index ? "bg-red-600" : "bg-slate-700"}`} onClick={() => seatBookingHandler(index)} key={seat}>{seat}</div>)
                 }
             </div>
+            {selectedSeat ? <p className='p-2 my-2'>Selected Seat No : {selectedSeat}</p> : null}
         </>
     )
 }
