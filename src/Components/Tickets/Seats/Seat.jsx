6f15905f1420e81cfbc5a90daf2ba93b5acd676a
aktaruzzaman-shaon <aktaruzzamanshaon69@gmail.com>
2025-01-07 17:50:50 +0600

    timer operation

--- src/Components/Tickets/Seats/Seat.jsx
+++ src/Components/Tickets/Seats/Seat.jsx
@@ -1,37 +1,44 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Seat = (props) => {
 
     const { seatQuantity } = props
     const [selectedSeat, setSelectedSeat] = useState([])
-    const [selectedSeatArrayIndex, setselectedSeatArrayIndex] = useState([])
 
     const seatsArray = [];
-
     for (let i = 1; i < seatQuantity; i++) {
         seatsArray.push(i)
     }
 
+    useEffect(() => {
+        const seatTimeInterval = setInterval(() => {
+            selectedSeat.pop()
+        }, 3000)
+
+        return clearInterval(seatTimeInterval)
+    }, selectedSeat)
+
     const seatBookingHandler = (index) => {
-        setSelectedSeat((prev) => [...prev, index])
-        setTimeout(() => {
-            setSelectedSeat([])
-        }, 9000);
-        if (selectedSeatArrayIndex) {
-            console.log("Entering")
-            setselectedSeatArrayIndex(0)
-        } else {
-            setselectedSeatArrayIndex(prev => prev + 1)
-            console.log("second stage")
-        }
+        // setTimeout(() => {
+        //     selectedSeat.pop()
+        //     console.log("poped")
+        //     console.log(selectedSeat)
+        // }, 3000);
+
+
+        setSelectedSeat(prevIndexes => prevIndexes.includes(index)
+            ? prevIndexes.filter((i) => i !== index)
+            : [...prevIndexes, index]
+        )
     }
 
+    console.log(selectedSeat)
     return (
         <>
             <div className='flex flex-row mx-auto justify-center space-x-1 mt-5'>
                 {
                     seatsArray.map((seat, index) =>
-                        <div className={`p-2  w-20 rounded-md ${selectedSeat[selectedSeatArrayIndex] === index ? "bg-green-600" : "bg-orange-600"}`} onClick={() => seatBookingHandler(index)} key={seat}>{seat}</div>
+                        <div className={`p-2  w-20 rounded-md ${selectedSeat.includes(index) ? "bg-green-600" : "bg-orange-600"}`} onClick={() => seatBookingHandler(index)} key={seat}>{seat}</div>
                     )
                 }
             </div>
