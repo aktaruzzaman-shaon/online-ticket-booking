563ddd04ae8611ebaec0587d9f7800c4777dd65f
aktaruzzaman-shaon <aktaruzzamanshaon69@gmail.com>
2025-01-20 12:18:39 +0600

    seat selection fully functional

--- src/Components/Tickets/Seats/Seat.jsx
+++ src/Components/Tickets/Seats/Seat.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const Seat = (props) => {
 
     const { seatQuantity } = props
     const [selectedSeat, setSelectedSeat] = useState([])
+    const intervalId = useRef()
 
     const seatsArray = [];
     for (let i = 1; i < seatQuantity; i++) {
@@ -11,34 +12,57 @@ const Seat = (props) => {
     }
 
     useEffect(() => {
-        const seatTimeInterval = setInterval(() => {
-            selectedSeat.pop()
-        }, 3000)
+        if (selectedSeat.length > 0) {
+            intervalId.current = setInterval(() => {
+                removeSelectedSeat()
+            }, 5000)
+        }
 
-        return clearInterval(seatTimeInterval)
-    }, selectedSeat)
+        if (selectedSeat.length === 0) {
+            clearInterval(intervalId.current)
+        }
 
-    const seatBookingHandler = (index) => {
-        // setTimeout(() => {
-        //     selectedSeat.pop()
-        //     console.log("poped")
-        //     console.log(selectedSeat)
-        // }, 3000);
+        return () => clearInterval(intervalId.current)
+    }, [selectedSeat])
+    // const seatBookingHandler = (index) => {
+    //     (() => {
+    //         setSelectedSeat((item) => item.pop())
+    //         console.log(selectedSeat)
 
+    //     }, 3000);
 
-        setSelectedSeat(prevIndexes => prevIndexes.includes(index)
-            ? prevIndexes.filter((i) => i !== index)
-            : [...prevIndexes, index]
-        )
+    //     setSelectedSeat(prevIndexes => prevIndexes.includes(index)
+    //         ? prevIndexes.filter((i) => i !== index)
+    //         : [...prevIndexes, index]
+    //     )
+    // }
+
+    const removeSelectedSeat = () => {
+
+        setSelectedSeat((prev) => {
+            const [removedSeat, ...restSelectedSeat] = prev
+            return restSelectedSeat
+        })
     }
 
-    console.log(selectedSeat)
+    const seatBookingHandler = (index) => {
+
+        //after clicking selected seat
+        if (selectedSeat.includes(index)) {
+            setSelectedSeat((prev) => prev.filter((el) => el !== index))
+        } else {
+            setSelectedSeat((prev) => [...prev, index])
+        }
+    }
+
+    console.log(selectedSeat, "double clicked seleced seat")
+
     return (
         <>
             <div className='flex flex-row mx-auto justify-center space-x-1 mt-5'>
                 {
                     seatsArray.map((seat, index) =>
-                        <div className={`p-2  w-20 rounded-md ${selectedSeat.includes(index) ? "bg-green-600" : "bg-orange-600"}`} onClick={() => seatBookingHandler(index)} key={seat}>{seat}</div>
+                        <div className={`p-2  w-20 rounded-md ${selectedSeat.includes(index) ? "bg-green-600 " : "bg-orange-600"} ${selectedSeat.length < 4 ? "cursor-auto" : "cursor-not-allowed"}`} onClick={selectedSeat.length < 4 ? () => seatBookingHandler(index) : undefined} key={seat}>{seat}</div>
                     )
                 }
             </div>
